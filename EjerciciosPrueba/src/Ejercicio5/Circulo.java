5847e781bcdcee356dbaa20df16fd1f9481971b3
p-pbalairon <p-pbalairon@P-PBALAIRON.neoris.cxnetworks.net>
2023-03-28 10:00:09 +0200

    commit 3

--- EjerciciosPrueba/src/Ejercicio5/Circulo.java
+++ EjerciciosPrueba/src/Ejercicio5/Circulo.java
@@ -3,6 +3,12 @@ package Ejercicio5;
 public class Circulo {
 
 	public static void main(String[] args) {
+		float radio = 23, area, longitud;
 		
+		area = (float) (3.14 *(radio*radio));
+		longitud = (float) (2*(3.14)*radio);
+		
+		System.out.println("El area es: " + area);
+		System.out.println("La longitud es: " + longitud);
 	}
 }
