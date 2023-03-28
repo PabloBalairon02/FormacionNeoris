89e75c1c965f01d837c810929ef719e8068d7908
p-pbalairon <p-pbalairon@P-PBALAIRON.neoris.cxnetworks.net>
2023-03-28 10:10:15 +0200

    arreglar ejercicio factura

--- EjerciciosPrueba/src/Ejercicio4/Factura.java
+++ EjerciciosPrueba/src/Ejercicio4/Factura.java
@@ -3,18 +3,18 @@ package Ejercicio4;
 public class Factura {
 
 	public static void main(String[] args) {
-		int cantidad = 0, unidades = 0, total = 0;
-		double precio= 0, importe = 0;
+		int cantidad = 3, unidades = 12, total = 0;
+		double precio= 5.4, importe = 0;
 		
 		importe = cantidad*unidades*precio;
 		total = (int) (importe * 1.21);
 		
 		
-		System.out.println("Cantidad " + "\t" + cantidad);
-		System.out.println("Cantidad " + "\t" + unidades);
-		System.out.println("Precio " + "\t" + precio);
-		System.out.println("Importe " + "\t" + importe);
-		System.out.println("Iva " + "\t" + (importe*0.21));
-		System.out.println("Total " + "\t" + total);
+		System.out.println("Cantidad: " + cantidad);
+		System.out.println("Cantidad: " + unidades);
+		System.out.println("Precio: " + precio);
+		System.out.println("Importe: " + importe);
+		System.out.println("Iva: " + (importe*0.21));
+		System.out.println("Total: " + total);
 	}
 }
